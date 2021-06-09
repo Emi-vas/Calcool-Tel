074923ff9183604f9c7cde71625a0c801652eb6c
Emilien Vaschalde <emilienvaschalde@yahoo.fr>
2021-06-09 08:38:15 +0200

    fin

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 window.onload = function() {
     let nb1 = 0
     let nb2 = 0
-    let rep = "0"
-    let good = 0
+    let rep = ""
     let score = 0
+
+    let t1 = "0"
+    let t2 = "0"
+    let t3 = "0"
+    let t4 = "0"
+    let t5 = "0"
+    let t6 = "0"
+    let t7 = "0"
+    let t8 = "0"
+    let t9 = "0"
+
+
     
     nb1 = arrondi()
     nb2 = arrondi()
@@ -46,52 +57,109 @@ window.onload = function() {
 
     }
 
-    function tempo() {
-        if (rep == nb1 * nb2 || good == 1) {
-
-        }else {
-        redLight()
-            
+    function print() {
         document
-           .getElementById("rep").value = (''); 
-        document
-           .getElementById("rep").setAttribute("disabled", true);
-        }
+        .getElementById("affichage")
+        .innerText = rep;
     }
 
-    window.setTimeout(tempo, 5000) // execute une seul fois
+
+
     
-    document.addEventListener("keydown", function(event) {
-        if (event.key == "Enter") {
+    document
+        .getElementById("touche1")
+        .addEventListener("click", function() {
+            rep = rep + "1"
+            print()
+            window.navigator.vibrate(200);
+        })
+
+    document
+        .getElementById("touche2")
+        .addEventListener("click", function() {
+            rep = rep + "2"
+            print()
+            window.navigator.vibrate(200);
+        })
+    document
+        .getElementById("touche3")
+        .addEventListener("click", function() {
+            rep = rep + "3"
+            print()
+            window.navigator.vibrate(200);
+        })
+    document
+        .getElementById("touche4")
+        .addEventListener("click", function() {
+            rep = rep + "4"
+            print()
+            window.navigator.vibrate(200);
+        })
+    document
+        .getElementById("touche5")
+        .addEventListener("click", function() {
+            rep = rep + "5"
+            print()
+            window.navigator.vibrate(200);
+        })
+    document
+        .getElementById("touche6")
+        .addEventListener("click", function() {
+            rep = rep + "6"
+            print()
+            window.navigator.vibrate(200);
+        })
+    document
+        .getElementById("touche7")
+        .addEventListener("click", function() {
+            rep = rep + "7"
+            print()
+            window.navigator.vibrate(200);
+        })
+    document
+        .getElementById("touche8")
+        .addEventListener("click", function() {
+            rep = rep + "8"
+            print()
+            window.navigator.vibrate(200);
+        })
+    document
+        .getElementById("touche9")
+        .addEventListener("click", function() {
+            rep = rep + "9"
+            print()
+            window.navigator.vibrate(200);
+        })
+    document
+        .getElementById("touche0")
+        .addEventListener("click", function() {
+            rep = rep + "0"
+            print()
+            window.navigator.vibrate(200);
+        })
+    document
+        .getElementById("clear")
+        .addEventListener("click", function() {
+            rep = ""
+            print()
+            window.navigator.vibrate(200);
+        })
+
+
+
+    document
+        .getElementById("toucheV")
+        .addEventListener("click", function() {
+            window.navigator.vibrate(200);
             if (rep == nb1 * nb2) {
                 greenLight()
-                
-                window.setTimeout(reset, 700) // execute une seul fois
-                document.getElementById("anima").classList.remove("anim")
-                good = 1
-                score++
-            } else if(rep == "") {
+                window.setTimeout(reset, 700)
+            } else {
                 redLight()
-            }else{
-                redLight()
-                document
-                   .getElementById("rep").value = "";
             }
-            
-        }
-    })
-    
-    document.getElementById("rep").addEventListener("input", function(e) {
-        rep = e.target.value
-    });
+        })
 
 
-
-    document.getElementById("reload").addEventListener("click", function() {
-        reset()
-    });
-
-    console.log(score)
 
 }
 
